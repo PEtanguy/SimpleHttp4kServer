caa179c594057cde4b56a58d95016c1ad2ecd96c
PETanguy <pierre.emmanuel.tanguy@gmail.com>
2025-04-07 15:02:56 +0100

    change name answer

--- src/test/kotlin/com/oocode/AnswererTest.kt
+++ src/test/kotlin/com/oocode/AnswererTest.kt
@@ -8,7 +8,7 @@ class AnswererTest {
     @Test
     fun `can answer name`() {
         val contents = Answerer().answerFor("What is your name?")
-        assertThat(contents, equalTo("Tom Cruise"))
+        assertThat(contents, equalTo("Johnny Depp"))
     }
 
     @Test
